76139b66a62879f0794dd75762bb07de5d43fe2a
Bhanuka Gihan <bhanukagihan4@gmail.com>
2025-09-18 16:11:31 +0530

    get elements on popup.js

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+const runningSpan = document.getElementById("runningSpan");
+const stoppedSpan = document.getElementById("stoppedSpan");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+
+const locationId = document.getElementById("locationId");
+const startDate = document.getElementById("startDate");
+const endDate = document.getElementById("endDate");
+
+const locationIdError = document.getElementById("locationIdError");
+const startDateError = document.getElementById("startDateError");
+const endDateError = document.getElementById("endDateError");
+
